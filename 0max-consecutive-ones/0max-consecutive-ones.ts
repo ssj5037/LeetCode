8301012260557b08d3f340f54e91b7378d5fba60
Sujin Shin <67135103+ssj5037@users.noreply.github.com>
2023-12-08 12:12:45 +0900

    Time: 65 ms, Memory: 45.4 MB - LeetHub

--- 0max-consecutive-ones/0max-consecutive-ones.ts
+++ 0max-consecutive-ones/0max-consecutive-ones.ts
@@ -2,14 +2,12 @@ function findMaxConsecutiveOnes(nums: number[]): number {
     let maxNum = 0;
     let sum = 0;
     nums.forEach((item, index) => {
-        if(item === 0) {
-            maxNum = maxNum < sum ? sum : maxNum;
-            sum = 0;
-        } else {
+        if (item === 1) {
             sum++;
         }
-        if (index === nums.length - 1) {
+        if (item === 0 || index === nums.length - 1) {
             maxNum = maxNum < sum ? sum : maxNum;
+            sum = 0;
         }
     })
     return maxNum;
