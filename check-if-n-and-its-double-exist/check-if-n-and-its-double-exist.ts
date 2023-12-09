acbb43efb3071ad2ba8d10ddf45d16edbb6cee7f
Sujin Shin <67135103+ssj5037@users.noreply.github.com>
2023-12-09 16:53:35 +0900

    Time: 59 ms, Memory: 44.7 MB - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.ts
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.ts
@@ -1,8 +1,8 @@
 function checkIfExist(arr: number[]): boolean {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length; j++) {
-            if (i === j) continue;
             if (arr[i] === 2 * arr[j]) {
+                if (i === j) continue;
                 return true;
             }
         }
