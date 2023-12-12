e4f7d3e4f57bbecb404815895159d7bbd675e824
Sujin Shin <67135103+ssj5037@users.noreply.github.com>
2023-12-12 16:59:58 +0900

    Time: 76 ms (9.09%), Space: 47.1 MB (9.60%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.ts
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.ts
@@ -1,9 +1,5 @@
 function maxProduct(nums: number[]): number {
-    nums.sort((x, y) => {
-        if (x > y) return -1;
-        else if (x < y) return 1;
-        return 0;
-    })
+    nums.sort((x, y) => y - x);
     console.log(nums);
     return (nums[0] - 1) * (nums[1] -1);
 };
