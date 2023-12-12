def3b68956e8319752ad60cda67b692ee63a30ad
Sujin Shin <67135103+ssj5037@users.noreply.github.com>
2023-12-12 18:51:12 +0900

    Time: 59 ms (77.81%), Space: 45.7 MB (63.88%) - LeetHub

--- 0136-single-number/0136-single-number.ts
+++ 0136-single-number/0136-single-number.ts
@@ -1,10 +1,9 @@
 function singleNumber(nums: number[]): number {
-    if (nums.length === 1) return nums[0];
-    let excepts: number[] = [];
-    for (let n in nums) {
-        let index = excepts.indexOf(nums[n]);
-        if (index > -1) excepts.splice(index, 1);
-        else excepts.push(nums[n]);
-    }
-    return excepts[0];
+    // 예: [2, 2, 1]의 경우 2^2^1 이 되므로,
+    // 2^2 = 0
+    // 2^2^1 = 1
+    // 결국 같은 수는 xor 연산자에 의해 0이 되고, 0과 남은 숫자끼리의 xor 연산은 남은 숫자가 나오게 된다.
+    
+    // 예2 : [4,1,2,1,2] => 4^1^2^1^2 = 4^0^0 = 4
+    return nums.reduce((x, y) => x^y);
 };
