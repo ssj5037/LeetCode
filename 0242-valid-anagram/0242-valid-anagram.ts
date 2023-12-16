2f21d0a5dc3d0909a0844165f3c9dd5464be9960
Sujin Shin <67135103+ssj5037@users.noreply.github.com>
2023-12-16 16:20:02 +0900

    Time: 90 ms (24.56%), Space: 49.1 MB (27.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,11 +1,13 @@
 function isAnagram(s: string, t: string): boolean {
-    if (s.length !== t.length) return false;
-    let sList = [...s];
-    sList.sort();
-    let tList = [...t];
-    tList.sort();
-    for(let i = 0; i < sList.length; i++) {
-        if (sList[i] !== tList[i]) return false;
-    }
-    return true;
+    // if (s.length !== t.length) return false;
+    // let sList = [...s];
+    // sList.sort();
+    // let tList = [...t];
+    // tList.sort();
+    // for(let i = 0; i < sList.length; i++) {
+    //     if (sList[i] !== tList[i]) return false;
+    // }
+    // return true;
+    
+    return s.split('').sort().join('') === t.split('').sort().join('');
 };
